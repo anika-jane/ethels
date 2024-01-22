d36ecabde77007ad1b9f2502c849f6f8a1cc575b
anika-jane <112495457+anika-jane@users.noreply.github.com>
2024-01-22 16:31:57 -0500

    added carousel

--- ethels/Components/Carousel/Carousel.tsx
+++ ethels/Components/Carousel/Carousel.tsx
@@ -1,25 +1,54 @@
-import React from "react";
-import Image from "next/image";
-import styles from "./Carousel.module.css";
-import Link from "next/link";
+"use client";
+import React, { useRef, useEffect } from "react";
+import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
-export const Carousel = () => {
+import styles from "./Carousel.module.css";
+
+import { Mousewheel, Pagination } from "swiper/modules";
+import Image from "next/image";
+
+register();
+
+export default function App() {
   return (
-    <section className={styles["CarouselContainer"]}>
-      <div className="slider">
-        <div className="slides">
-          <div id="slide-1">1</div>
-          <div id="slide-2">2</div>
-          <div id="slide-3">3</div>
-          <div id="slide-4">4</div>
-          <div id="slide-5">5</div>
-        </div>
-        <Link href="#slide-1">1</Link>
-        <Link href="#slide-2">2</Link>
-        <Link href="#slide-3">3</Link>
-        <Link href="#slide-4">4</Link>
-        <Link href="#slide-5">5</Link>
-      </div>
-    </section>
+    <>
+      <Swiper
+        style={{
+          "--swiper-pagination-color": "#e14747",
+          "--swiper-pagination-bullet-inactive-color": "#fac88d",
+          "--swiper-pagination-bullet-inactive-opacity": "1",
+          "--swiper-pagination-bullet-size": "12px",
+          "--swiper-pagination-bullet-horizontal-gap": "4px",
+        }}
+        spaceBetween={30}
+        mousewheel={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Mousewheel, Pagination]}
+        className={styles["SwiperContainer"]}
+      >
+        <SwiperSlide>
+          <Image
+            src="/Assets/Images/the-interior 2.png"
+            className={styles["SwiperImage"]}
+            height={500}
+            width={500}
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
-};
+}
--- ethels/src/app/page.tsx
+++ ethels/src/app/page.tsx
@@ -6,11 +6,13 @@ import { Specials } from "../../Components/Specials/Specials";
 import { About } from "../../Components/About/About";
 import { Instagram } from "../../Components/Instagram/Instagram";
 import { Menu } from "../../Components/Menu/Menu";
+import App from "../../Components/Carousel/Carousel";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <App />
       <Legendary />
       <Instagram />
       <Menu />
