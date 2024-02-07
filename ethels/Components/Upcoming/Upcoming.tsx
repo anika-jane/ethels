344c33b9572abc5fdd1cc2ea04d481c6272ac6d2
anika-jane <112495457+anika-jane@users.noreply.github.com>
2024-02-07 11:59:06 -0500

    Updated the Upcoming Events Section

--- ethels/Components/Upcoming/Upcoming.tsx
+++ ethels/Components/Upcoming/Upcoming.tsx
@@ -12,7 +12,7 @@ export const Upcoming = () => {
         <div className={styles["EventCard"]}>
           <div className={styles["ImageContainer"]}>
             <Image
-              src="/Assets/Events/tray-race-notfinal.jpg"
+              src="/Assets/Images/MardiGras.jpeg"
               height={166}
               width={255}
               alt=""
@@ -21,17 +21,116 @@ export const Upcoming = () => {
           </div>
           <div className={styles["CardText"]}>
             <div className={styles["EventInfo"]}>
-              <p className={styles["EventName"]}>Tray Race</p>
-              <p className={styles["EventDate"]}>August 2023</p>
+              <p className={styles["EventName"]}>Mardi Gras Menu</p>
+              <p className={styles["EventDate"]}>Feb 13-17</p>
             </div>
             <div className={styles["CardBody"]}>
               <p className={styles["EventDescription"]}>
-                For about 25 years, the annual Ethel’s tray race has been
-                raising funds for local charities. In 2023, 22 teams
-                participated and battled it out in the gruelling event in which
-                teams must balance water bottles on a tray while taking part in
-                a relay race. Two-thousand dollars was donated to Dog Pound
-                Rescue, a foster home-based rescue serving Waterloo Region.
+                <p className={styles["FeatureName"]}>
+                  Gumbo &#160;&#160;&#160;$6/$8
+                </p>
+                <p className={styles["FeatureDes"]}>
+                  {" "}
+                  Made with smoked kielbasa, shrimp and braised chicken. This
+                  spicy creole dish features the iconic flavours of sweet
+                  peppers and okra. Served over dirty rice with a side of
+                  cornbread.
+                </p>
+
+                <br />
+                <p className={styles["FeatureName"]}>
+                  Muffuletta Sandwich &#160;&#160;&#160;$14
+                </p>
+                <p className={styles["FeatureDes"]}>
+                  Layers of sliced soppressata, mortadella, capicola and
+                  provolone cheese, served on a fresh focaccia bun, topped with
+                  giardiniera + olive spread. Served with cajun potato wedges.
+                </p>
+                <br />
+                <p className={styles["FeatureName"]}>
+                  Roast Beef Po'Boy &#160;&#160;&#160;$15
+                </p>
+                <p className={styles["FeatureDes"]}>
+                  A New Orleans classic. Slow roasted beef chuck, shredded
+                  lettuce, tomato, dill pickle + mayonnaise served on a french
+                  style loaf.
+                </p>
+                <br />
+                <p className={styles["FeatureName"]}>
+                  Jambalaya &#160;&#160;&#160;$16
+                </p>
+                <p className={styles["FeatureDes"]}>
+                  Smoked turkey and kielbasa, braised with sweet bell pepper,
+                  tomato + onions. Served over dirty rice with a side of
+                  cornbread.
+                </p>
+                <br />
+                <p className={styles["FeatureName"]}>
+                  Bread Pudding &#160;&#160;&#160;$8
+                </p>
+                <p className={styles["FeatureDes"]}>
+                  A rich and decadent bread pudding, topped with raisin rum
+                  custard cream and served with a scoop of vanilla ice cream.
+                </p>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles["EventCard"]}>
+          <div className={styles["ImageContainer"]}>
+            <Image
+              src="/Assets/Images/Screenshot 2024-02-07 at 11.50.30.png"
+              height={166}
+              width={255}
+              alt=""
+              className={styles["EventImage"]}
+            />
+          </div>
+          <div className={styles["CardText"]}>
+            <div className={styles["EventInfo"]}>
+              <p className={styles["EventName"]}>Maple Leaf Suitcase Party</p>
+              <p className={styles["EventDate"]}>March 16</p>
+            </div>
+            <div className={styles["CardBody"]}>
+              <p className={styles["EventDescription"]}>
+                Head on over to Ethel's for your chance to win 2 tickets and
+                travel expenses to see the Toronto Maple Leafs vs. Caroline
+                Hurricanes on March 16th.
+                <br />1 ticket per entry
+                <br /> Must be present to win, and ready to go!
+                <br />
+                Draw at 3:00pm
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles["EventCard"]}>
+          <div className={styles["ImageContainer"]}>
+            <Image
+              src="/Assets/Images/FishFry.png"
+              height={166}
+              width={255}
+              alt=""
+              className={styles["EventImage"]}
+            />
+          </div>
+          <div className={styles["CardText"]}>
+            <div className={styles["EventInfo"]}>
+              <p className={styles["EventName"]}>Good Friday Fish Fry</p>
+              <p className={styles["EventDate"]}>March 29</p>
+            </div>
+            <div className={styles["CardBody"]}>
+              <p className={styles["EventDescription"]}>
+                Ethel's Good Friday Fish Fry, enjoy 2 5oz pieces of haddock,
+                potato wedges, slaw and tartar sauce for $21! Dine-in or place
+                your order for take-out{" "}
+                <a
+                  href="https://order2.silverwarepos.com/app/EthelsLounge#!/menu"
+                  target="_blank"
+                  className={styles["FryLink"]}
+                >
+                  online.
+                </a>
               </p>
             </div>
           </div>
@@ -48,17 +147,14 @@ export const Upcoming = () => {
           </div>
           <div className={styles["CardText"]}>
             <div className={styles["EventInfo"]}>
-              <p className={styles["EventName"]}>Event Name</p>
-              <p className={styles["EventDate"]}>Event Date</p>
+              <p className={styles["EventName"]}>Cinco de Mayo Menu</p>
+              <p className={styles["EventDate"]}>May 1-5</p>
             </div>
             <div className={styles["CardBody"]}>
               <p className={styles["EventDescription"]}>
-                For about 25 years, the annual Ethel’s tray race has been
-                raising funds for local charities. In 2023, 22 teams
-                participated and battled it out in the gruelling event in which
-                teams must balance water bottles on a tray while taking part in
-                a relay race. Two-thousand dollars was donated to Dog Pound
-                Rescue, a foster home-based rescue serving Waterloo Region.
+                Get ready for everyone's favourite menu of the year!
+                <br />
+                Drool worthy details coming soon.
               </p>
             </div>
           </div>
@@ -66,7 +162,7 @@ export const Upcoming = () => {
         <div className={styles["EventCard"]}>
           <div className={styles["ImageContainer"]}>
             <Image
-              src="/Assets/Events/tray-race-notfinal.jpg"
+              src="/Assets/Images/Hosebagen.jpg"
               height={166}
               width={255}
               alt=""
@@ -75,44 +171,15 @@ export const Upcoming = () => {
           </div>
           <div className={styles["CardText"]}>
             <div className={styles["EventInfo"]}>
-              <p className={styles["EventName"]}>Event Name</p>
-              <p className={styles["EventDate"]}>Event Date</p>
+              <p className={styles["EventName"]}>Oktoberfest Menu</p>
+              <p className={styles["EventDate"]}>Oct 8-13</p>
             </div>
             <div className={styles["CardBody"]}>
               <p className={styles["EventDescription"]}>
-                For about 25 years, the annual Ethel’s tray race has been
-                raising funds for local charities. In 2023, 22 teams
-                participated and battled it out in the gruelling event in which
-                teams must balance water bottles on a tray while taking part in
-                a relay race. Two-thousand dollars was donated to Dog Pound
-                Rescue, a foster home-based rescue serving Waterloo Region.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={styles["EventCard"]}>
-          <div className={styles["ImageContainer"]}>
-            <Image
-              src="/Assets/Events/tray-race-notfinal.jpg"
-              height={166}
-              width={255}
-              alt=""
-              className={styles["EventImage"]}
-            />
-          </div>
-          <div className={styles["CardText"]}>
-            <div className={styles["EventInfo"]}>
-              <p className={styles["EventName"]}>Event Name</p>
-              <p className={styles["EventDate"]}>Event Date</p>
-            </div>
-            <div className={styles["CardBody"]}>
-              <p className={styles["EventDescription"]}>
-                For about 25 years, the annual Ethel’s tray race has been
-                raising funds for local charities. In 2023, 22 teams
-                participated and battled it out in the gruelling event in which
-                teams must balance water bottles on a tray while taking part in
-                a relay race. Two-thousand dollars was donated to Dog Pound
-                Rescue, a foster home-based rescue serving Waterloo Region.
+                Zigge Zagge, Zigge Zagge, Yum Yum Yum!
+                <br />
+                Check back to see what Bavarian delights Ethel cooks up this
+                year.
               </p>
             </div>
           </div>
